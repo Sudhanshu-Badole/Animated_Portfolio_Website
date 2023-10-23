e22c7efe2ad5952615f4011f0a617898e2ff551b
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-24 00:35:30 +0530

    4-React Framer Motion

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import "./navbar.scss"
+import React from 'react';
+import "./navbar.scss";
+import {motion} from "framer-motion";
 const Navbar = () => {
   return (
     <div className='navbar'>
         {/* sidebar */}
 <div className="wrapper">
-    <span>Sudhanshu</span>
+    <motion.span
+    initial={{ opacity: 0, scale: 0.5}}
+    animate={{ opacity: 1, scale: 1}}
+    transition={{ duration:0.5}}>
+        Sudhanshu
+        </motion.span>
     <div className="social">
         <a href="#">
             <img src="/facebook.png" alt="" />
