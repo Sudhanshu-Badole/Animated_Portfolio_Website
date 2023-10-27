7d0f09053f3efb8c047d9d16be3044b3b059a390
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-27 09:36:29 +0530

    8-Hero Section Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.scss"
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return <div>
     <section id="Homepage">
       <Navbar/>
+      <Hero/>
     </section>
     <section><a href="#">Hero</a></section>
     <section id="Services">Parallax</section>
