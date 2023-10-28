6555635218d47997e0e44630d32d5f0ec8b8607f
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-29 00:27:30 +0530

    10-Services Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
     <section id="Homepage">
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
     </section>
-    <section id="Services"><Parallax type="services" /></section>
-    <section>Services</section>
+    <section id="Services">
+      <Parallax type="services" />
+    </section>
+    <section>
+      <Services/>
+    </section>
     <section id="Portfolio"><Parallax type="portfolio" /></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
