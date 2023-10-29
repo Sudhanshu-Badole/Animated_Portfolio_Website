39de0b5ed47c37ff4b76d0cb8a7cb4d58603b16f
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-29 07:24:43 +0530

    11-Portfolio Section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
@@ -16,10 +17,10 @@ const App = () => {
     <section>
       <Services/>
     </section>
-    <section id="Portfolio"><Parallax type="portfolio" /></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <section id="Portfolio">
+      <Parallax type="portfolio" />
+      </section>
+    <Portfolio/>
     <section id="Contact">contact</section>
   </div>;
 };
